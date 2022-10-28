77b2fe2ab57e2d283a98a76fe98f10ef8d188ae9
unknown <jkub.sliwinski@gmail.com>
2022-10-28 11:05:47 +0200

    Library App

--- src/main/java/com/library/util/SystemUtils.java
+++ src/main/java/com/library/util/SystemUtils.java
@@ -6,7 +6,7 @@ public class SystemUtils {
     public static Connection connection;
     private static final String URL = "jdbc:mysql://localhost:3306/library";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "ciastko123";
+    private static final String PASSWORD = "";
 
     public static void connectToDatabase() {
 
